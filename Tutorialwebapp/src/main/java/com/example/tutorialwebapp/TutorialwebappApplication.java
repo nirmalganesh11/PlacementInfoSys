e82cc402de369dcc526fb46625d44c99792be27a
nirmalganesh11 <2000031363@kluniversity.in>
2022-02-11 21:02:05 +0530

    Spring Index
    First Commit about to be reduce the frontend

---
+++ Tutorialwebapp/src/main/java/com/example/tutorialwebapp/TutorialwebappApplication.java
@@ -0,0 +1,22 @@
+package com.example.tutorialwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+public class TutorialwebappApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TutorialwebappApplication.class, args);
+    }
+
+}
+
+
+
+
+
+
---
+++ Tutorialwebapp/src/test/java/com/example/tutorialwebapp/TutorialwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
